767b75f5af803fd4af67da7b4419580dfbcbdf8c
Mariusz Kogen <k0gen@pm.me>
2023-04-03 23:17:31 +0200

    * Add catch-all location block to handle client-side routing   * Add gzip compression

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -17,16 +17,48 @@ server {
     ssl_certificate /mnt/cert/main.cert.pem;
     ssl_certificate_key /mnt/cert/main.key.pem;
 
-    server_name  localhost;
+    server_name localhost;
+
+    root /usr/share/nginx/html;
+    index index.html index.htm;
+
+    # Gzip settings
+    gzip on;
+    gzip_disable "msie6";
+    gzip_vary on;
+    gzip_proxied any;
+    gzip_comp_level 6;
+    gzip_buffers 16 8k;
+    gzip_http_version 1.1;
+    gzip_min_length 256;
+    gzip_types
+        application/atom+xml
+        application/geo+json
+        application/javascript
+        application/x-javascript
+        application/json
+        application/ld+json
+        application/manifest+json
+        application/rdf+xml
+        application/rss+xml
+        application/xhtml+xml
+        application/xml
+        font/eot
+        font/otf
+        font/ttf
+        image/svg+xml
+        text/css
+        text/javascript
+        text/plain
+        text/xml;
 
     location / {
-        root   /usr/share/nginx/html;
-        index  index.html index.htm;
+        try_files $uri $uri/ /index.html;
     }
 
-    error_page   500 502 503 504  /50x.html;
+    error_page 500 502 503 504 /50x.html;
     location = /50x.html {
-        root   /usr/share/nginx/html;
+        root /usr/share/nginx/html;
     }
 }
 '
